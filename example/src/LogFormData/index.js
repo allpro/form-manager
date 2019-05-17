89346d7a69b8030aee841ad2086a855a0d29a720
Kevin Dalman <development@allpro.net>
2019-05-16 20:15:05 -0700

    Refine implementation of @allpro/demo-layout

--- example/src/LogFormData/index.js
+++ example/src/LogFormData/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
 
 import FormManager from '@allpro/form-manager'
 import { FieldsTestOutput } from '@allpro/form-manager-devtools'
@@ -16,6 +17,18 @@ class LongFormDemo extends Component {
 	render() {
 		return (
 			<div style={{ maxWidth: '800px', margin: '0 auto' }}>
+				<Typography variant="h6">
+					NOTE: The <code>FieldsTestOutput</code> helper is in a
+					separate dev-helpers package to avoid bloating FormManager:
+					<a
+						href="https://www.npmjs.com/package/@allpro/form-manager-devtools"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Form-Manager-DevTools
+					</a>
+				</Typography>
+
 				<FieldsTestOutput form={this.form} />
 			</div>
 		)
